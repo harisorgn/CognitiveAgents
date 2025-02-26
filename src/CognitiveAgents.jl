dc89d5d20d3a57ac24e432f37442d18acc032a4e
Haris Orgn <organtzh@gmail.com>
2025-02-26 12:47:47 +0200

    update exports

--- src/CognitiveAgents.jl
+++ src/CognitiveAgents.jl
@@ -5,6 +5,8 @@ using Distributions
 using CommonRLInterface
 import CommonRLInterface: reset!, actions, observe, terminated, act!
 
+using Turing
+
 using Optimization
 using Optim
 
@@ -37,7 +39,7 @@ include("plot.jl")
 export read_data_bipolar, read_data_psychopy, read_data_js 
 export get_choices, get_correct_categories, get_stimuli, get_response_times
 export GridSearch, CLResult, CMResult, EMAgent, CategoryLearnEnv
-export initialise_agent, objective, fit_model, run_trial!, run_task!
+export initialise_agent, objective, fit_model, fit_bayes, run_trial!, run_task!
 export plot_subject_accuracy, plot_group_accuracy, plot_group_accuracy_per_set, plot_rt
 
 end 
