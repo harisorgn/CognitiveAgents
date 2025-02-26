aaca9153b75dd67762fd261b59de4e4ec163379a
Haris Orgn <organtzh@gmail.com>
2025-02-26 12:47:38 +0200

    add script for turing parameter recovery

--- run_inverse.jl
+++ run_inverse.jl
@@ -1,6 +1,6 @@
 using CognitiveAgents
 using Serialization
-using OptimizationNLopt
+using Turing
 
 function fit_batch(IDs, session, run, algs, grid_sz, σ_conv; kwargs...)
     for ID in IDs
@@ -45,13 +45,32 @@ filter!(r -> r.phase == "test", df)
 
 #IDs = unique(df.subject_id)
 IDs = [9, 36, 38, 40, 41, 43]
-alg = NLopt.GN_MLSL_LDS()
 
 session = "glc"
 run = 1
+
+#df_fit = df[(df.subject_id .== 9) .& (df.run .== run) .& (df.session .== session), :]
+df_fit = df[(df.subject_id .== 9), :]
+
+using CognitiveAgents: get_loglikelihood_dots, get_choices, get_response_dots, discrete_evidence
+
+L = get_loglikelihood_dots(df_fit)
+C = get_choices(df_fit)
+RD = get_response_dots(df_fit)
+
+model = discrete_evidence(RD, L, C)
+sig, P_lapse, choices = model()
+
+model = discrete_evidence(RD, L, choices)
+chain = sample(model, Emcee(40), 2_000, progress=false)
+chain_prior = sample(model, Prior(), 2_000, progress=false)
+
+res = fit_bayes(df_fit)
+
+#alg = NLopt.GN_CRS2_LM()
 for ID in IDs
     df_fit = df[(df.subject_id .== ID) .& (df.run .== run) .& (df.session .== session), :]
-    res = fit_model(df, alg)
+    res = fit_model(df_fit, alg)
 
     filename = "CL_res_subj-$(ID)_ses-$(session)_run-$(run).jls"
     serialize(filename, res)
