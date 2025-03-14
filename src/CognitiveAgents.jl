f7904e5d5ec3508df14578e6101f131dae84d8c9
Haris Orgn <organtzh@gmail.com>
2025-03-14 19:28:37 +0200

    update exports

--- src/CognitiveAgents.jl
+++ src/CognitiveAgents.jl
@@ -42,8 +42,8 @@ include("plot.jl")
 export read_data_bipolar, read_aggressiveness, read_data_psychopy, read_data_js
 export get_choices, get_correct_categories, get_stimuli, get_response_times
 export GridSearch, CLResult, CMResult, EMAgent, CategoryLearnEnv
-export objective, category_match, fit_CM, fit_CM, fit_faces, run_trial!, run_task!
+export objective, category_match, fit_CL, fit_CM, fit_CM, fit_faces, run_CL_task
 export plot_subject_accuracy, plot_group_accuracy, plot_group_accuracy_per_set, plot_rt
-export figure_CM_psychophysics, figure_faces_psychophysics, figure_faces_RT
+export figure_CM_psychophysics, figure_faces_psychophysics, figure_faces_RT, figure_subject_accuracy
 
 end 
