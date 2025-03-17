35d3ef84b23dddb06cb8e92ef6133f4577cba4ef
Haris Orgn <organtzh@gmail.com>
2025-03-17 19:00:35 +0200

    add utility functions and update regressor extraction

--- src/category_learn.jl
+++ src/category_learn.jl
@@ -1,8 +1,3 @@
-
-probability_right_category(stim, w) = logistic(w' * stim)
-
-probability_right_category(stim, w, β) = logistic(β * w' * stim)
-
 struct CLResult
     sol
     image_size
@@ -10,6 +5,18 @@ struct CLResult
     subject_ID
     session
     run
+end
+
+probability_right_category(stim, w) = logistic(w' * stim)
+
+probability_right_category(stim, w, β) = logistic(β * w' * stim)
+
+function loglikelihood_category(P_right_cat, cat)
+    return cat * log(P_right_cat + eps()) + (1 - cat) * log(1 - P_right_cat + eps())
+end
+
+function prediction_error(correct_cat, predicted_cat, η)
+    return η * (correct_cat - predicted_cat)
 end
 
 function objective(S::AbstractMatrix, choices::AbstractVector, corrects::AbstractVector, p)
@@ -31,7 +38,7 @@ function objective(S::AbstractMatrix, choices::AbstractVector, corrects::Abstrac
 
         llhood += loglikelihood_category(P_right, choice_cat)
 
-        weights .+= η * (correct_cat - P_right) .* stimulus 
+        weights .+= stimulus .* prediction_error(correct_cat, P_right, η) 
     end
 
     return -llhood
@@ -44,28 +51,14 @@ function fit_CL(df, alg; σ_conv=5, grid_sz=(50,50), kwargs...)
 
     lb = [0, 0]
     ub = [1.0, Inf]
-    
-    if alg isa GridSearch
-        step_sz = alg.step_size
-        r1 = lb[1]:step_sz:ub[1]
-        r2 = lb[2]:step_sz:ub[2]
-        OBJ = zeros(length(r1), length(r2))
-        for (i, p1) in enumerate(r1)
-            for (j, p2) in enumerate(r2)
-                OBJ[i, j] = obj([p1, p2], nothing)
-            end
-        end
 
-        sol = OBJ
-    else
-        obj = OptimizationFunction(
-            (p, hyperp) -> objective(S, choices, corrects, p), 
-            Optimization.AutoForwardDiff()
-        )
-        p0 = [0.1, 1]
-        prob = OptimizationProblem(obj, p0, lb = lb, ub = ub)
-        sol = solve(prob, alg; kwargs...)
-    end
+    obj = OptimizationFunction(
+        (p, hyperp) -> objective(S, choices, corrects, p), 
+        Optimization.AutoForwardDiff()
+    )
+    p0 = [0.1, 1]
+    prob = OptimizationProblem(obj, p0, lb = lb, ub = ub)
+    sol = solve(prob, alg; kwargs...)
 
     id = unique(df.subject_id)
     session = unique(df.session)
@@ -76,15 +69,12 @@ function fit_CL(df, alg; σ_conv=5, grid_sz=(50,50), kwargs...)
     return res
 end
 
-function run_CL_task(df::DataFrame, res::CLResult)
-    corrects = get_correct_categories(df)
-    S = get_stimuli(df; grid_sz = res.image_size, σ_conv = res.σ_convolution)
+function run_CL_task(S::AbstractMatrix, corrects::AbstractVector, res::CLResult)
     η, β = res.sol
-
     D, N_trials = size(S)
     weights = zeros(typeof(η), D)
 
-    choices = zeros(N_trials)
+    choices = zeros(Int, N_trials)
     iscorrects = zeros(Bool, N_trials)
     for t in Base.OneTo(N_trials)
         stimulus = S[:,t]
@@ -97,45 +87,66 @@ function run_CL_task(df::DataFrame, res::CLResult)
         weights .+= η * (correct_cat - P_right) .* stimulus 
     end
 
-    df_sim = copy(df)
-    df_sim.response .= choices
-    df_sim.correct .= iscorrects
+    df = DataFrame(response = choices, correct = iscorrects)
 
+    return df
+end
+
+function run_CL_task(df::DataFrame, res::CLResult; N_runs = 1)
+    corrects = get_correct_categories(df)
+    S = get_stimuli(df; grid_sz = res.image_size, σ_conv = res.σ_convolution)
+   
+    df_sim = mapreduce(append!, Base.OneTo(N_runs)) do _
+        df_choices = run_CL_task(S, corrects, res)
+        df_temp = copy(df)
+        df_temp.response = df_choices.response
+        df_temp.correct = df_choices.correct
+        df_temp
+    end
+    
     return df_sim
 end
 
 function results_to_regressors(res::CLResult, df)
     df_regress = DataFrame(
-        t_loglikelihood = Float64[],
+        t = Float64[],
         t_RPE_pos = Float64[],
         t_RPE_neg = Float64[], 
-        loglikelihood = Float64[], 
+        P_chosen = Float64[], 
+        P_unchosen = Float64[], 
+        P_left = Float64[], 
+        P_right = Float64[], 
         RPE_pos = Float64[],
         RPE_neg = Float64[]
     )
 
-    df_fit = df[(df.subject_id .== res.subject_ID) .& (df.run .== res.run) .& (df.session .== res.session), :]
+    subject_ID = only(res.subject_ID)
+    run = only(res.run)
+    session = only(res.session)
+
+    df_fit = @subset(df, :subject_id .== subject_ID, :run .== run, :session .== session)
     ST = parse.(Float64, df_fit.stim_presentation_time)
     RT = parse.(Float64, df_fit.response_time)
 
-    C = choices(df_fit)
-    X = stimuli(df_fit; grid_sz=res.image_size, σ_conv=res.σ_convolution)
-    D, N_trials = size(X)
+    choices = get_choicesp1(df_fit)
+    corrects = get_corrects(df_fit)
+    S = get_stimuli(df_fit; grid_sz=res.image_size, σ_conv=res.σ_convolution)
+    D, N_trials = size(S)
 
-    ag = initialise_agent(X, res.sol)
-    tm = TrialMetrics(N_trials)
-    EM_learning!(ag, X, C, tm)
-
+    η, β = res.sol
+    weights = zeros(typeof(η), D)
     for t in Base.OneTo(N_trials)
-        
-        z_stim = ag.z_stim[t]
-        loglikelihood = tm.loglhood[t][z_stim] 
+        stimulus = S[:,t]
+        choice_cat = choices[t] 
+        correct_cat = corrects[t]
 
-        z = ag.z[t]
-        w = t == 1 ? zeros(D) : tm.W[t-1][:, z]
-        logq = tm.logq[t][z]
-        pred_cat = probability_right_category(X[:, t], w)
-        RPE = prediction_error(C[t], pred_cat, ag.η, logq)
+        P_right = probability_right_category(stimulus, weights, β)
+        Ps = [1 - P_right, P_right]
+        idx_unchosen = choice_cat == 2 ? 1 : 2
+
+        RPE = prediction_error(correct_cat, P_right, η)
+
+        weights .+= η * (correct_cat - P_right) .* stimulus
 
         if RPE > 0.0
             t_RPE_pos = ST[t] + RT[t]
@@ -152,17 +163,20 @@ function results_to_regressors(res::CLResult, df)
         push!(
             df_regress, 
             (
-                t_loglikelihood = ST[t], 
+                t = ST[t], 
                 t_RPE_pos = t_RPE_pos,
                 t_RPE_neg = t_RPE_neg, 
-                loglikelihood = loglikelihood, 
+                P_chosen = Ps[choice_cat], 
+                P_unchosen = Ps[idx_unchosen], 
+                P_left = Ps[1], 
+                P_right = Ps[2], 
                 RPE_pos = RPE_pos,
                 RPE_neg = RPE_neg
             )
         )
     end
 
-    CSV.write("CL_regress_sub-$(res.subject_ID)_ses-$(res.session)_run-$(res.run).csv", df_regress)
+    CSV.write("CL_regress_sub-$(subject_ID)_ses-$(session)_run-$(run).csv", df_regress)
 end
 
 function results_to_dataframe(results::CLResult)
