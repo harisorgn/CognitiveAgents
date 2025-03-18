2267a52afe0b9ddbb9e1dc8a0fb28e185927fde4
Haris Orgn <organtzh@gmail.com>
2025-03-18 17:48:44 +0200

    update faces script

--- run_faces.jl
+++ run_faces.jl
@@ -1,6 +1,7 @@
 using CognitiveAgents
 using Serialization
 using OptimizationOptimJL
+using DataFramesMeta
 
 cols = [
     :subject_id,
@@ -20,11 +21,20 @@ df = read_data_bipolar(files, cols)
 
 IDs = unique(df.subject_id)
 
-df_fit = df[(df.subject_id .== 11) .& (df.run .== 1) .& (df.session .== "bhb"), :]
-
+run = 1
+session = "glc"
+IDs = unique(df.subject_id)
 alg = Optim.IPNewton()
-res = fit_faces(df_fit, alg)
+for ID in IDs
+    df_fit = @subset(df, :subject_id .== ID, :run .== run, :session .== session)
 
-figure_faces_psychophysics(df_fit, res; save_fig=true)
+    res = fit_faces(df_fit, alg)
+    
+    serialize("faces_model_sub-$(ID)_ses-$(session)_run-$(run).csv", res)
 
-figure_faces_RT(df_fit, res; save_fig=true)
+    results_to_regressors(res, df)
+end
+
+#figure_faces_psychophysics(df_fit, res; save_fig=true)
+
+#figure_faces_RT(df_fit, res; save_fig=true)
